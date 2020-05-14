5fc71509a326190e79940a8f30e98681683eec3d
MR-SKYFALL <46224840+MR-SKYFALL@users.noreply.github.com>
2020-05-14 17:54:08 +0200

    contact form - frontend

--- JS/contact-page.js
+++ JS/contact-page.js
@@ -4,7 +4,7 @@ function open_nav_action(nav_bar) {
     document.querySelector(".menu-js").classList.add("animation-open-menu");
 
     if (document.querySelector('html').clientWidth > 700) {
-        document.querySelector(".container").classList.add("animation-center-cards-after-open-menu");
+        document.querySelector(".contact").classList.add("animation-center-cards-after-open-menu");
     }
     if (document.querySelector('html').clientWidth < 450) {
         nav_bar.classList.add("animate-open-navbar-small-device");
@@ -18,30 +18,46 @@ function close_nav_action(nav_bar) {
 
     document.querySelector(".menu-js").classList.remove("animation-open-menu");
     document.querySelector(".menu-js").classList.add("animation-close-menu");
-    document.querySelector(".container").classList.remove("animation-center-cards-after-open-menu");
+    document.querySelector(".contact").classList.remove("animation-center-cards-after-open-menu");
     nav_bar.classList.remove("animate-open-navbar-small-device");
     nav_bar.classList.remove("animation-open-navbar");
     // var input_checkbox = document.querySelector("#toggle-nav");
     // input_checkbox.checked = false;
 }
+document.addEventListener('DOMContentLoaded', () => {
 
-var back_all_card_and_nav_div = document.querySelector(".back-all-card-js"); // action for clicking on the background and then closing the navbar and cards
+    var send_button = document.querySelector(".contact__btn-js"); // action for send to me button
 
-back_all_card_and_nav_div.addEventListener("click", function () {
-    var all_card_front = document.querySelectorAll(".front-side-js");
+    send_button.addEventListener("click", function () {
+        document.querySelector(".contact__front-js").classList.add("animation-rotate-card-front");
+        document.querySelector(".contact__back-js").classList.add("animation-rotate-card-back");
+    })
 
-    all_card_front.forEach(card_front => {
-        card_front.parentNode.querySelector('.front-side-js').classList.remove("animation-rotate-card-front");
-        card_front.parentNode.querySelector('.back-side-js').classList.remove("animation-rotate-card-back");
-    });
+    var send_button = document.querySelector(".contact__back-icon-js"); // action for back to contact
 
-    // var hamburger = document.querySelector(".hamburger-js");
-    var navbar = document.querySelector(".navbar-js");
-    var input_checkbox = document.querySelector("#toggle-nav");
-    input_checkbox.checked = false;
-    close_nav_action(navbar);
+    send_button.addEventListener("click", function () {
+        document.querySelector(".contact__front-js").classList.remove("animation-rotate-card-front");
+        document.querySelector(".contact__back-js").classList.remove("animation-rotate-card-back");
+    })
 
-})
+
+    var back_all_card_and_nav_div = document.querySelector(".back-all-card-js"); // action for clicking on the background and then closing the navbar and cards
+
+    back_all_card_and_nav_div.addEventListener("click", function () {
+        var all_card_front = document.querySelectorAll(".front-side-js");
+
+        all_card_front.forEach(card_front => {
+            card_front.parentNode.querySelector('.front-side-js').classList.remove("animation-rotate-card-front");
+            card_front.parentNode.querySelector('.back-side-js').classList.remove("animation-rotate-card-back");
+        });
+
+        // var hamburger = document.querySelector(".hamburger-js");
+        var navbar = document.querySelector(".navbar-js");
+        var input_checkbox = document.querySelector("#toggle-nav");
+        input_checkbox.checked = false;
+        close_nav_action(navbar);
+
+    })
 
     var hamburger = document.querySelector(".hamburger-js"); // action for open/close navbar
 
@@ -59,3 +75,4 @@ back_all_card_and_nav_div.addEventListener("click", function () {
 
 
     })
+})
