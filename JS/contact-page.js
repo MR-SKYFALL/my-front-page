5718ed74198ab805d04de54026666bcb9cfa7202
MR-SKYFALL <46224840+MR-SKYFALL@users.noreply.github.com>
2020-05-15 18:24:18 +0200

    clipboard correction

--- JS/contact-page.js
+++ JS/contact-page.js
@@ -1,3 +1,17 @@
+var clipboard = new ClipboardJS('.copy-button-js');
+clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+
+    e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
+
 function open_nav_action(nav_bar) {
 
     document.querySelector(".menu-js").classList.remove("animation-close-menu");
