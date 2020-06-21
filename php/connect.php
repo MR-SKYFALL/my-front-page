46420e11c368dea46d8ed3250a72cfdc184996ed
Dawid Matras <46224840+MR-SKYFALL@users.noreply.github.com>
2020-06-21 14:23:18 +0200

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,6 +1,6 @@
 <?php
-$host = "23181.m.tld.pl";
-$db_user = "admin23181_dawid_kontakt";
-$db_password = "9StXenZeGM";
-$db_name = "baza23181_dawid_kontakt";
+$host = "";
+$db_user = "";
+$db_password = ";
+$db_name = "";
 ?>
